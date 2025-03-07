c23b042af14eeaee7adde92d796a01502908eebf
Terraxuka <youmlian404@gmail.com>
2025-03-07 09:21:22 +0100

    My change

--- src/DebugDiv.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DebugDiv({ value, onChange }) {
-    return (
-        <input className='DebugDiv' type="number" value={value} onChange={(e) => onChange(e.target.value)} />
-    );
-}
-
-export default DebugDiv;
